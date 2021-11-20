a5e01e92d9670ca8f941becf0bc8c3a70a899c35
Ubuntu <ubuntu@ip-172-31-15-186.ap-south-1.compute.internal>
2021-11-20 14:53:34 +0000

    text updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world1");
-    System.out.println("Hello world2");
-    System.out.println("Hello world3");
-    System.out.println("Hello world4");
-    System.out.println("Hello world5");
+    System.out.println("Hello DevOps world1");
+    System.out.println("Hello DevOps world2");
+    System.out.println("Hello DevOps world3");
+    System.out.println("Hello DevOps world4");
+    System.out.println("Hello DevOps world5");
   }
 }
